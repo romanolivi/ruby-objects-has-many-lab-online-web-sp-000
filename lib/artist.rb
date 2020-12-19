2a077e47784b06603f4ce84adeebd5697afb593e
Romano Livi <romano_livi@outlook.com>
2020-12-19 20:47:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,8 +21,11 @@ class Artist
   
   def add_song_by_name(name)
     song = Song.new(name)
-    add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
   end
+
   
   def self.song_count
     @@song_count
