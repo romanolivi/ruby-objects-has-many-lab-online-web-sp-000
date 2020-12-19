22d508aa9fca2ff2267c4bc23cdfaccd8b981e6f
Romano Livi <romano_livi@outlook.com>
2020-12-19 19:50:36 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,16 @@
+class Author 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts
+    Post.all.select {|p| p.author == self}
+  end
+  
+  def add_post(post)
+    post = Author.name
+  
+end
